6e40af06b3e69f1aa0781b56c561494ae4a3c7dd
J Lorieau <justin@lorieau.com>
2023-08-21 12:42:28 -0500

    Add sphinx design and matplolib dependencies

--- source/conf.py
+++ source/conf.py
@@ -13,15 +13,30 @@ author = 'Chem 343 Instructors'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.mathjax",
+    "sphinx_design",
+    "matplotlib.sphinxext.plot_directive",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/latex.html
+
+latex_elements = {
+    'preamble': r'''
+\usepackage{mhchem}
+'''}
+
+# -- Options for plot extension ----------------------------------------------
+plot_formats = ['png', 'pdf']
+plot_html_show_source_link = False
+plot_html_show_formats = False
